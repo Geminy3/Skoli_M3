f0c14d3f52da7014a94b264256cf20b2a0da5fe0
amassein <aljo.m@icloud.com>
2023-05-12 15:21:38 +0200

    Scrap du site de M3

--- PDF2TXT.py
+++ PDF2TXT.py
@@ -10,43 +10,68 @@ export_folder = "export2txt"
 pdf_folder = "./dump/articles/"
 error = []
 
-print("PDFMINER.SIX")
-for i, article in enumerate(os.listdir(pdf_folder)):
-    print(f"\r{i}/{len(os.listdir(pdf_folder))} : {article}", end = "")
+def pdf2txt(path, output = False):
+
     doc = []
-    if export_folder not in os.listdir("./"):
-        os.mkdir(export_folder)
-    try:
-        for page_layout in extract_pages(pdf_folder+article):
-            for element in page_layout:
-                if isinstance(element, LTTextContainer):
-                    doc.append((element.get_text().replace('\n', " ").replace("\'", ' ')))
+    for page_layout in extract_pages(path):
+                for element in page_layout:
+                    if isinstance(element, LTTextContainer):
+                        doc.append((element.get_text().replace('\n', " ").replace("\'", ' ')))
+    if output:
+        print(doc)
 
-        # print("".join(doc))
-        with open(export_folder+"/"+article.replace("pdf", "txt"), "w", encoding="utf-8") as writefile:
-            writefile.writelines("".join(doc))
-    except:
-        print(f"{article} not in folder ?")
-        for page_layout in extract_pages(pdf_folder+article):
-            print("thre is a layout")
-            for element in page_layout:
-                if isinstance(element, LTTextContainer):
-                    print("there is an instance")
-                else:
-                    print("there is no instance")
-        error.append(article)
+    return(doc)
+
+def pdfminer():
+
+    print("PDFMINER.SIX")
+    for i, article in enumerate(os.listdir(pdf_folder)):
+        print(f"\r{i}/{len(os.listdir(pdf_folder))} : {article}", end = "")
+
+        if export_folder not in os.listdir("./"):
+            os.mkdir(export_folder)
+        try:
+            doc = pdf2txt(pdf_folder+article)
+
+            with open(export_folder+"/"+article.replace("pdf", "txt"), "w", encoding="utf-8") as writefile:
+                writefile.writelines("".join(doc))
+        except:
+            print(f"{article} not in folder ?")
+            for page_layout in extract_pages(pdf_folder+article):
+                print("thre is a layout")
+                for element in page_layout:
+                    if isinstance(element, LTTextContainer):
+                        print("there is an instance")
+                    else:
+                        print("there is no instance")
+            error.append(article)
+
+def pypdf2txt(path, output = False):
+
+    doc = []
+    reader = PdfReader(path)
+    for pages in reader.pages:
+        doc.append(pages.extract_text().split("\n"))
+
+    if output:
+        print(doc)
+
+    return(doc)
+
+def pypdf():
     
+    print("PyPDF2")
+    for article in os.listdir(pdf_folder)[:1]:
+        if export_folder not in os.listdir("./"):
+            os.mkdir(export_folder)
+        try:
+            doc = pypdf2txt(pdf_folder+article)
+        except:
+            print(f"{article} not in folder ?")
+    print(doc)
 
-# print("PyPDF2")
-# for article in os.listdir(pdf_folder)[:1]:
-#     doc = []
-#     if export_folder not in os.listdir("./"):
-#         os.mkdir(export_folder)
-#     try:
-#         reader = PdfReader(pdf_folder+article)
-#         print(article)
-#         for pages in reader.pages:
-#             doc.append(pages.extract_text().split("\n"))
-#     except:
-#         print(f"{article} not in folder ?")
-# print(doc)
+if __name__ == "__main__":
+    #pdfminer()
+    #pdf2txt(pdf_folder+"008b617a213cda74dacb384f11abf187.pdf",  output = True)
+    #pypdf2txt(pdf_folder+"008b617a213cda74dacb384f11abf187.pdf",  output = True)
+    print("change script to convert pdf to txt")
