516e8b4dd883f88528e8f42c0c1caaa30bad4de9
amassein <aljo.m@icloud.com>
2023-06-14 13:23:00 +0200

    Build dash app

---
+++ app.py
@@ -0,0 +1,36 @@
+from dash import Dash, html, dcc, Output, Input
+import dash_bootstrap_components as dbc
+import dash
+import plotly.express as px
+import pandas as pd
+
+
+app = Dash(__name__, use_pages=True, external_stylesheets=[dbc.themes.SPACELAB])
+
+sidebar = dbc.Nav(
+            [
+                dbc.NavLink(
+                    [
+                        html.Div(page["name"], className="ms-2"),
+                    ],
+                    href=page["path"],
+                    active="exact",
+                )
+                for page in dash.page_registry.values()
+            ],
+            vertical=True,
+            pills=True,
+            justified=True,
+            fill=True,
+)
+
+app.layout = dbc.Container([
+    dbc.Row([
+        dbc.Col(sidebar, width = 2),
+        dbc.Col(dash.page_container)
+        ]),
+    ], 
+    fluid=True)
+
+if __name__ == '__main__':
+	app.run_server(debug=True)
---
+++ pages/Auteurs.py
@@ -0,0 +1,41 @@
+from dash import Dash, html, dcc
+import dash
+# import dash_core_components as dcc 
+# import dash_html_components as html 
+from dash.dependencies import Input, Output
+import plotly.express as px
+import dash_bootstrap_components as dbc
+import pandas as pd
+#from side_bar import sidebar
+
+dash.register_page(__name__)
+
+# assume you have a "long-form" data frame
+# see https://plotly.com/python/px-arguments/ for more options
+df = pd.DataFrame({
+    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+    "Amount": [4, 1, 2, 2, 4, 5],
+    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
+})
+
+fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+
+main_div = html.Div(children=[
+    html.Br(),
+    html.H1(children="Analyse sur les auteurs"),
+
+    html.Div(children=f'''
+         A web application framework for your data.
+    '''),
+
+    dcc.Graph(
+        id='example-graph',
+        figure=fig
+    )
+])
+
+def layout():
+    return(html.Div([
+        main_div  # adjust width
+            ])
+    )
