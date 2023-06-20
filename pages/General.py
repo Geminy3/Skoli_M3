b62da203e123ba91e832d151668e5a327f68e3ec
amassein <aljo.m@icloud.com>
2023-06-20 14:24:34 +0200

    Update General

--- pages/General.py
+++ pages/General.py
@@ -13,9 +13,10 @@ df = pd.read_csv("data/data_article_clean.csv")
 
 def graph_time():
     
-    filtered_df = pd.DataFrame({"values" : df.YEAR.value_counts().sort_index(),
-                       "year" : df.YEAR.value_counts().sort_index().index})
-    fig = px.area(filtered_df, x="year", y="values")
+    tmp = pd.crosstab(df.YEAR, df.TYPE).melt(ignore_index=False)
+    tmp["YEAR"] = tmp.index
+    tmp = tmp.sort_values(by="value")
+    fig = px.area(tmp, x="YEAR", y="value", color="TYPE")
     return(fig)
 
 def graph_tag_nb():
@@ -28,6 +29,8 @@ def graph_tag_nb():
                        "year" : tmp.tags.value_counts().sort_index().index})
 
     fig = px.bar(filtered_df, x="year", y="values")
+    fig.update_layout(barmode='stack', xaxis={'categoryorder':'total descending'})
+
     return(fig)
 
 def graph_tag_time():
@@ -47,6 +50,48 @@ def graph_tag_time():
 
     fig = px.bar(temp, x="tag", y="value", color="YEAR")
     fig.update_layout(barmode='stack', xaxis={'categoryorder':'total descending'})
+
+    return(fig)
+
+def area_tag():
+    
+    tag = pd.read_csv("./res/tags.csv", index_col=0)
+    tmp = tag.set_index("url").join(df.set_index("URL")[["YEAR", "DATE", "TYPE"]])
+    tmp = tmp.loc[tmp.DATE.notna()]
+    f_tmp = tmp[tmp.YEAR >= 1998]
+    filtered_tmp = f_tmp.groupby("tags").filter(lambda x:len(x) >= 150)
+    
+    tmp = pd.crosstab(filtered_tmp.YEAR, filtered_tmp.tags)
+    temp = pd.crosstab(f_tmp.YEAR, f_tmp.tags)
+    z = list(temp.columns.drop(tmp.columns))
+    idx = [temp.columns.get_loc(col) for col in z]
+    tmp["else"] = temp.iloc[:,idx].sum(axis=1)
+    
+    tmp = tmp.transpose().transform(lambda x:x/sum(x)).mul(100).transpose().melt(ignore_index=False)
+    tmp["YEAR"] = tmp.index
+    fig = px.area(tmp, x="YEAR", y="value", color="tags",
+                  color_discrete_sequence=px.colors.qualitative.Dark24)
+    
+    return(fig)
+
+def area_tag2():
+    
+    tag = pd.read_csv("./res/tags.csv", index_col=0)
+    tmp = tag.set_index("url").join(df.set_index("URL")[["YEAR", "DATE", "TYPE"]])
+    tmp = tmp.loc[tmp.DATE.notna()]
+    f_tmp = tmp[tmp.YEAR >= 1998]
+    filtered_tmp = f_tmp.groupby("tags").filter(lambda x:len(x) < 150 and len(x) >= 90)
+    
+    tmp = pd.crosstab(filtered_tmp.YEAR, filtered_tmp.tags)
+    temp = pd.crosstab(f_tmp.YEAR, f_tmp.tags)
+    z = list(temp.columns.drop(tmp.columns))
+    idx = [temp.columns.get_loc(col) for col in z]
+    tmp["else"] = temp.iloc[:,idx].sum(axis=1)
+    
+    tmp = tmp.transpose().transform(lambda x:x/sum(x)).mul(100).transpose().melt(ignore_index=False)
+    tmp["YEAR"] = tmp.index
+    fig = px.area(tmp, x="YEAR", y="value", color="tags",
+                  color_discrete_sequence=px.colors.qualitative.Dark24)
     
     return(fig)
 
@@ -62,13 +107,27 @@ main_div = html.Div([
     ]),
     html.Div([
         html.H3("Informations relatives aux tags"),
-        html.H5("Nombre de tags"),
-        dcc.Graph(figure=graph_tag_nb(), id="GgenTagNB")
+        # html.H5("Nombre de tags"),
+        # dcc.Graph(figure=graph_tag_nb(), id="GgenTagNB")
     ]),
     html.Div([
-        html.H5("Tags dans le temps"),
+        html.H5("Nombre de tags"),
         dcc.Graph(figure=graph_tag_time())
-    ])#, style = {'width': '49%', 'display': 'inline-block'})
+    ]),#, style = {'width': '49%', 'display': 'inline-block'}),
+    html.Div([
+        html.H5("Tag dans le temps"),
+        html.Div([
+            html.P(html.B("Top 15")),
+            dcc.Graph(figure=area_tag(), id="area_tag"),
+        ], style = {'width': '49%', 'display': 'inline-block'}),
+        html.Div([
+            html.P(html.B("Next 15")),
+            dcc.Graph(figure=area_tag2(), id="area_tag2"),
+        ], style={'width': '49%', 'float': 'right', 'display': 'inline-block'}),
+    ]),
+    html.Div([
+        html.H3("The big Bla")
+    ])
 ])
 
 # @callback(
