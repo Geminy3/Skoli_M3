44e56ee3c20712d5e53ebfbd032777200463ce0d
Geminy3 <aljo.m@icloud.com>
2023-11-28 13:59:30 +0100

    5th update

---
+++ Lib/DATA2NET.py
@@ -0,0 +1,336 @@
+
+import networkx as nx
+from bs4 import BeautifulSoup
+import requests
+import re
+import json
+import pandas as pd
+import os
+import numpy as np
+
+EXPORT_FOLDER = "./res/"
+IMPORT_FOLDER = "./data2/"
+
+# TAGS
+
+with open(IMPORT_FOLDER+"data_items.json", "r", encoding="utf8") as f:
+    mult_items = json.loads(f.read())
+
+def tag_net(EXPORT_FOLDER, mult_items):
+
+    tag_dic = {}
+
+    for url in mult_items:
+        # print(url, len(mult_items[url]["tags"]))
+        for tag in mult_items[url]["tags"]:
+            if tag == "N" or tag == "A" or tag == '\n   \tThéo BATTAGLIA\n':
+                pass
+            elif tag not in tag_dic.keys():
+                tag_dic[tag] = {}
+
+    for tag in tag_dic.keys():
+        tag_dic[tag]["value"] = 0
+        for url in mult_items:
+            if tag in mult_items[url]["tags"]:
+                tag_dic[tag]["value"] += 1
+                for t in mult_items[url]["tags"]:
+                    if t in tag_dic.keys():
+                        if t != tag and t not in tag_dic[tag].keys():
+                            tag_dic[tag][t] = {"weight" : 1}
+                        elif t != tag and t in tag_dic[tag].keys():
+                            tag_dic[tag][t]["weight"] += 1
+
+
+    G = nx.Graph()
+
+    for url in mult_items:
+        for tag in mult_items[url]["tags"]:
+            if tag not in G.nodes():
+                G.add_node(tag, value = 1)
+            elif tag in G.nodes():
+                G.nodes[tag]["value"] += 1
+
+    for node in G.nodes:
+        for url in mult_items:
+            if node in mult_items[url]["tags"]:
+                for tag in mult_items[url]["tags"]:
+                    if node != tag and not G.has_edge(tag, node):
+                        G.add_edge(node, tag, weight = 1)
+                    elif node != tag and G.has_edge(tag, node):
+                        G[node][tag]["weight"] += 1
+
+    g = nx.Graph(tag_dic)
+    nx.draw_shell(G)
+    export = nx.cytoscape_data(G)
+    with open(EXPORT_FOLDER+"tag_net.json", "w", encoding="utf8") as f:
+        f.write(json.dumps(export))
+
+    value = []
+    for tag in tag_dic.keys():
+        value.append(tag_dic[tag]["value"])
+    val = pd.DataFrame(value)
+    val.index = tag_dic.keys()
+    val.to_csv(EXPORT_FOLDER+"nodes_attributes.csv")
+    print(json.dumps(export, indent = 4))
+
+# AUTEURS
+
+def auteurs_net(EXPORT_FOLDER, mult_items):
+
+
+    aut_dic = {}
+
+    for url in mult_items:
+        # print(url, len(mult_items[url]["tags"]))
+        for aut in mult_items[url]["auteurs"]:
+            if aut == "N" or aut == "A":
+                pass
+            else:
+                if aut not in aut_dic.keys():
+                    aut_dic[aut] = {}
+                    aut_dic[aut]["value"] = 1
+                elif aut in aut_dic.keys():
+                    aut_dic[aut]["value"] += 1
+
+
+    for aut in aut_dic.keys():
+        for url in mult_items:
+            if aut in mult_items[url]["auteurs"]:
+                # aut_dic[aut]["value"] += 1
+                for a in mult_items[url]["tags"]:
+                    if a != aut and a not in aut_dic[aut].keys():
+                        aut_dic[aut][a] = {"weight" : 1}
+                    elif a != aut and a in aut_dic[aut].keys():
+                        aut_dic[aut][a]["weight"] += 1
+
+
+    G = nx.Graph()
+
+    for url in mult_items:
+        for aut in mult_items[url]["auteurs"]:
+            if aut not in G.nodes():
+                G.add_node(aut, value = 1)
+            elif aut in G.nodes():
+                G.nodes[aut]["value"] += 1
+
+    for node in G.nodes:
+        for url in mult_items:
+            if node in mult_items[url]["auteurs"]:
+                for aut in mult_items[url]["auteurs"]:
+                    if node != aut and not G.has_edge(aut, node):
+                        G.add_edge(node, aut, weight = 1)
+                    elif node != aut and G.has_edge(aut, node):
+                        G[node][aut]["weight"] += 1
+
+    # g = nx.Graph(aut_dic)
+    # print(list(nx.connected_components(g)))
+    # print(nx.number_connected_components(g))
+    # nx.draw(g)
+
+    # nx.draw(G)
+    for i, item in enumerate(nx.connected_components(G)):
+        if len(item) > 2:
+            print(i, len(item), item)
+    export = nx.cytoscape_data(G)
+    with open(EXPORT_FOLDER+"aut_net.json", "w", encoding="utf8") as f:
+        f.write(json.dumps(export))
+
+    value = []
+    for aut in aut_dic.keys():
+        print(f"\r{aut_dic[aut]['value']}")
+        value.append(aut_dic[aut]["value"])
+    val = pd.DataFrame(value)
+    val.index = aut_dic.keys()
+    val.to_csv(EXPORT_FOLDER+"autnodes_attributes.csv")
+
+    g = nx.Graph(aut_dic)
+    d = nx.to_pandas_adjacency(g)
+    d.values[[np.arange(len(d))]*2] = np.nan
+    d = d.stack().reset_index()
+    d.columns = ["source", "target", "values"]
+    d = d[d["values"] > 0]
+    d.to_csv("./pages/data/aut2tag.csv")
+
+def tag2aut(EXPORT_FOLDER, mult_items):
+
+    G = nx.Graph()
+
+    for item in mult_items.values():
+        for aut in item["auteurs"]:
+            if aut == 'A' or aut == "N":
+                continue
+            if aut not in G.nodes:
+                G.add_node(aut, value = 1, type = "Auteurs")
+            elif aut in G.nodes:
+                G.nodes[aut]["value"] += 1
+        for tag in item["tags"]:
+            if tag == 'A' or tag == "N":
+                continue
+            if tag not in G.nodes:
+                G.add_node(tag, value = 1, type = "Tags")
+            elif tag in G.nodes:
+                G.nodes[tag]["value"] += 1
+
+    for i, node in enumerate(G.nodes):
+        print(f"\r{i, node}", end="")
+        for item in mult_items.values():
+            tags = item["tags"]
+            auts = item["auteurs"]
+            if node in tags:
+                for aut in auts:
+                    if aut == 'A' or aut == "N":
+                        continue
+                    if node != aut and not G.has_edge(aut, node):
+                        G.add_edge(node, aut, weight = 1)
+                    elif node != aut and G.has_edge(aut, node):
+                        G.edges[(node, aut)]["weight"] += 1
+                for tag in tags:
+                    if tag == 'A' or tag == "N":
+                        continue
+                    if node != tag and not G.has_edge(tag, node):
+                        G.add_edge(node, tag, weight = 1)
+                    elif node != tag and G.has_edge(tag, node):
+                        G.edges[(node, tag)]["weight"] += 1
+                continue
+            if node in auts:
+                for aut in auts:
+                    if aut == 'A' or aut == "N":
+                        continue
+                    if node != aut and not G.has_edge(aut, node):
+                        G.add_edge(node, aut, weight = 1)
+                    elif node != aut and G.has_edge(aut, node):
+                        G.edges[(node, aut)]["weight"] += 1
+                for tag in tags:
+                    if tag == 'A' or tag == "N":
+                        continue
+                    if node != tag and not G.has_edge(tag, node):
+                        G.add_edge(node, tag, weight = 1)
+                    elif node != tag and G.has_edge(tag, node):
+                        G.edges[(node, tag)]["weight"] += 1
+
+    export = nx.cytoscape_data(G)
+    with open(EXPORT_FOLDER+"all_net.json", "w", encoding="utf8") as f:
+        f.write(json.dumps(export))
+
+    value = []
+    type = []
+    for node in G.nodes:
+        value.append(G.nodes[node]["value"])
+        type.append(G.nodes[node]["type"])
+    items = zip(value, type)
+    val = pd.DataFrame(items, columns=["value", "type"])
+    val.index = G.nodes
+    val.to_csv(EXPORT_FOLDER+"allnodes_attributes.csv")
+
+    d = nx.to_pandas_adjacency(G)
+    d.values[[np.arange(len(d))]*2] = np.nan
+    d = d.stack().reset_index()
+    d.columns = ["source", "target", "values"]
+    d = d[d["values"] > 0]
+    d.to_csv("./pages/data/tag2aut.csv")
+
+def auteurs_net2(EXPORT_FOLDER, mult_items):
+
+    url_aut = {}
+    i = 0
+    for url in mult_items:
+        for aut in mult_items[url]["auteurs"]:
+            if aut == "N" or aut == "A":
+                pass
+            else:
+                tags = mult_items[url]["tags"]
+                url_aut[i] = {"auteurs" : aut, "url" : url, "tags" : "_".join(tags)}
+            i += 1
+    df = pd.DataFrame().from_dict(url_aut).transpose()
+    df = df.join(df.tags.str.split(pat='_',expand=True))
+    df.to_csv(EXPORT_FOLDER+"auteurs.csv")
+
+def tag_net2(EXPORT_FOLDER, mult_items):
+    
+    url_tag = {}
+    i = 0
+    for url in mult_items:
+        for tag in mult_items[url]["tags"]:
+            if tag == "N" or tag == "A":
+                pass
+            else:
+                auteurs = mult_items[url]["auteurs"]
+                url_tag[i] = {"tags" : tag, "url" : url, "auteurs" : "_".join(auteurs)}
+            i += 1
+    df = pd.DataFrame().from_dict(url_tag).transpose()
+    df = df.join(df.auteurs.str.split(pat='_',expand=True))
+    df.to_csv(EXPORT_FOLDER+"tags.csv")
+
+def auteurs_links(mult_items):
+
+    aut_dic = {}
+
+    for url in mult_items:
+        for aut in mult_items[url]["auteurs"]:
+            if aut == "N" or aut == "A":
+                pass
+            else:
+                if aut not in aut_dic.keys():
+                    aut_dic[aut] = {}
+                    
+    for aut in aut_dic.keys():
+        for url in mult_items:
+            if aut in mult_items[url]["auteurs"]:
+                # aut_dic[aut]["value"] += 1
+                for a in mult_items[url]["auteurs"]:
+                    if a != aut and a not in aut_dic[aut].keys():
+                        aut_dic[aut][a] = {"weight" : 1}
+                    elif a != aut and a in aut_dic[aut].keys():
+                        aut_dic[aut][a]["weight"] += 1
+
+    g = nx.Graph(aut_dic)
+    d = nx.to_pandas_adjacency(g)
+    d.values[[np.arange(len(d))]*2] = np.nan
+    d = d.stack().reset_index()
+    d.columns = ["source", "target", "values"]
+    d = d[d["values"] > 0]
+    d.to_csv("./pages/data/auteurs_link.csv")
+
+def tag_links(mult_items):
+
+    tag_dic = {}
+
+
+    for url in mult_items:
+        # print(url, len(mult_items[url]["tags"]))
+        for tag in mult_items[url]["tags"]:
+            if tag == "N" or tag == "A" or tag == '\n   \tThéo BATTAGLIA\n':
+                pass
+            else:
+                if tag not in tag_dic.keys():
+                    tag_dic[tag] = {}
+
+    for tag in tag_dic.keys():
+        for url in mult_items:
+            if tag in mult_items[url]["tags"]:
+                for t in mult_items[url]["tags"]:
+                    if t in tag_dic.keys():
+                        if t != tag and t not in tag_dic[tag].keys():
+                            tag_dic[tag][t] = {"weight" : 1}
+                        elif t != tag and t in tag_dic[tag].keys():
+                            tag_dic[tag][t]["weight"] += 1
+
+    g = nx.Graph(tag_dic)
+    d = nx.to_pandas_adjacency(g)
+    d.values[[np.arange(len(d))]*2] = np.nan
+    d = d.stack().reset_index()
+    d.columns = ["source", "target", "values"]
+    d = d[d["values"] > 0]
+    d.to_csv("./pages/data/tags_link.csv")
+
+def auteurs_db(df):
+    aut = pd.read_csv("./res/auteurs.csv", index_col=0)
+    tmp = aut.set_index("url").join(df.set_index("URL")[["YEAR", "DATE", "TYPE"]])
+    tmp = tmp.loc[tmp.DATE.notna()]
+    tmp.to_csv("./pages/data/auteurs_db.csv")
+
+def tags_db(df):
+    tag = pd.read_csv("./res/tags.csv", index_col=0)
+    tmp = tag.set_index("url").join(df.set_index("URL")[["YEAR", "DATE", "TYPE"]])
+    tmp = tmp.loc[tmp.DATE.notna()]
+    tmp.to_csv("./pages/data/tags_db.csv")
---
+++ Lib/TEXT2DATA.py
@@ -0,0 +1,104 @@
+import spacy
+#import spacy_transformers
+import pathlib
+import os
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+import glob
+import pandas as pd
+
+def get_lemma(nlp, df, EXPORT_FOLDER):
+
+    rows, cols = df.shape 
+
+    for row in range(rows):
+        txt = []
+        art = str(df.iloc[row].ARTICLE)
+        res = str(df.iloc[row].RESUME)
+        titre = str(df.iloc[row].TITRE)
+        id = df.iloc[row]["Unnamed: 0"]
+        
+        full = titre+'\n'+res+'\n'+art
+        doc2 = nlp(res+'\n'+art)
+        
+        for token in doc2:
+            if token.is_stop == False :#and token.pos_ != "PUNCT":
+                txt.append(token.lemma_)
+            
+        pathlib.Path(f'{EXPORT_FOLDER}Texte/{id}').mkdir(parents=True, exist_ok=True)
+        with open(f"{EXPORT_FOLDER}Texte/{id}/full.txt", "w", encoding="utf8") as f:
+            f.write(full)
+        with open(f"{EXPORT_FOLDER}Texte/{id}/texte.txt", "w", encoding="utf8") as f:
+            f.write(" ".join(txt))
+        
+        print(f"\rSuccessfuly exported {row} / {rows} : {txt[0:10]}", end="")
+    
+
+
+def instanciate_method(IMPORT_FOLDER, df):
+
+    fichiers = glob.glob(IMPORT_FOLDER+"Texte/*/texte.txt")
+    nOcc = {}
+    documents=[]
+    urls=[]
+    for fichier in fichiers:
+        idx = fichier.split("/")[3]
+        url = df[df["Unnamed: 0"] == int(idx)].URL.values[0]
+        urls.append(url)
+        with open(fichier, encoding='utf8') as f:
+            documents.append(f.read())
+    counter=CountVectorizer()
+    nOcc=counter.fit_transform(documents)
+    nOcc=pd.DataFrame.sparse.from_spmatrix(nOcc,index=urls,columns=counter.get_feature_names_out())
+
+    tfidfer=TfidfVectorizer(lowercase=False) #On ne maîtrise pas bien la tokennisation de CountVectorizer !
+    tfidf_sklearn=tfidfer.fit_transform(documents)
+    tfidf_sklearn=pd.DataFrame.sparse.from_spmatrix(tfidf_sklearn,index=urls,columns=tfidfer.get_feature_names_out())
+
+    return(nOcc, tfidf_sklearn)
+
+
+#### To export tag term_doc matrices
+def get_tags_term_doc(tfidf_sklearn, nOcc):
+
+    methods = ["tfidf", "Occ"]
+
+    for method in methods:
+        print(method)
+        if method == "tfidf":
+            method = tfidf_sklearn
+            export = "TF_IDF"
+        else:
+            method = nOcc
+            export = "Occurences"
+
+        tag_info = pd.read_csv("./pages/data/tags_db.csv", index_col=0)
+
+        for i, tag in enumerate(tag_info.tags.unique()):
+            tmp = tag_info[tag_info.tags == tag].index
+            print(tag, i, "/", len(tag_info.tags.unique()), "Nb_urls :", len(tmp))
+            tmp2 = method[method.index.isin(tmp)].sum()
+            tmp2 = tmp2[tmp2 > 0].sort_values(ascending=False)
+            tmp2.to_csv(f"./pages/data/Txt/Tags/{export}/{tag}.csv")
+
+def get_aut_term_doc(tfidf_sklearn, nOcc):
+
+    methods = ["tfidf", "Occ"]
+
+    for method in methods:
+        print(method)
+        if method == "tfidf":
+            method = tfidf_sklearn
+            export = "TF_IDF"
+        else:
+            method = nOcc
+            export = "Occurences"
+
+        aut_info = pd.read_csv("./pages/data/auteurs_db.csv", index_col=0)
+
+        for i, auteur in enumerate(aut_info.auteurs.unique()):
+            tmp = aut_info[aut_info.auteurs == auteur].index
+            print(auteur, i, "/", len(aut_info.auteurs.unique()), "Nb_urls :", len(tmp))
+            tmp2 = method[method.index.isin(tmp)].sum()
+            tmp2 = tmp2[tmp2 > 0].sort_values(ascending=False)
+            tmp2.to_csv(f"./pages/data/Txt/Auteurs/{export}/{auteur}.csv")
